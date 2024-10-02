931397e2bf6fb849df4fc19883045ea0e5b23461
MehwishRazzak786 <a.razzakmehwish786786@gmail.com>
2024-10-02 14:31:09 +0500

    class 1&2 practice

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer(){
+    return(
+        <h2 className="flex bg-blue-500 justify-center">All Rights Reserved By Mehwish Abdul Razzak </h2> 
+    )
+}
---
+++ src/app/about/page.tsx
@@ -0,0 +1,17 @@
+import Footer from "@/Components/Footer"
+import Link from "next/link"
+
+export default function About(){
+    return(
+        <>
+      <ul className="flex gap-40 bg-blue-500 justify-center">
+      <li ><Link href= "/">HOME</Link></li>
+      <li ><Link href= "/about">ABOUT</Link></li>
+      <li ><Link href= "/gallery">GALLERY</Link></li>
+      <li ><Link href="/contact-us">CONTACT US</Link></li>
+    </ul>
+    <h1 className="one">This Is About Page...</h1>
+    <Footer />
+        </>
+    )
+}
